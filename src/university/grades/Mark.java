02715474aafa1426f35ea79819848be1ee47f216
Мирас Нуралиев <mirasnuraliev@MacBook-Pro-Miras-2.local>
2024-12-10 18:24:42 +0500

    literalmarks added

--- src/university/grades/Mark.java
+++ src/university/grades/Mark.java
@@ -38,4 +38,20 @@ public class Mark {
     public double getTotalMark() {
         return firstAttestation + secondAttestation + finalExam;
     }
+
+    public String getLiteralMark() {
+        int a =  firstAttestation + secondAttestation + finalExam;
+        if(a>100){String t="A";return t;}
+        else if(a>=95 && a<=100){String t="A";return t;}
+        else if(a>=90 && a<=94){String t="A-";return t;}
+        else if(a>=85 && a<=89){String t="B+";return t;}
+        else if(a>=80 && a<=84){String t="B";return t;}
+        else if(a>=75 && a<=79){String t="B-";return t;}
+        else if(a>=70 && a<=74){String t="C+";return t;}
+        else if(a>=65 && a<=69){String t="C";return t;}
+        else if(a>=60 && a<=64){String t="C-";return t;}
+        else if(a>=55 && a<=59){String t="D+";return t;}
+        else if(a>=50 && a<=54){String t="D";return t;}
+        else {String t="F";return t;}
+    }
 }
